7d0c2982916e8014d087e6f675f4b75054aa50f4
Leon2ev <jevgeni.glazunov@gmail.com>
2023-03-01 13:45:53 +0100

    Add all types to index

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,41 @@ import createHiveAuth from './auth/index.js'
 import createHiveDomain from './domain/index.js'
 import createHiveService from './service/index.js'
 import createHiveGhost from './ghost/index.js'
+import type { AuthGetTokenParams } from './auth/types.js'
+import type {
+  DomainGetAllParams,
+  DomainCreateParams,
+  DomainDeleteParams,
+  DomainActivateParams,
+  DomainDeactivateParams,
+} from './domain/types.js'
+import type {
+  ServiceCreateParams,
+  ServiceDeleteParams,
+  ServiceGetAllParams,
+  ServiceGhostAddParams,
+  ServiceLinksParams,
+  ServiceLinksUpdateParams,
+  ServiceUpdateParams,
+} from './service/types.js'
+import type {
+  GhostAcceptPendingParams,
+  GhostAddMapIDParams,
+  GhostDeleteParams,
+  GhostDisableDataParams,
+  GhostGenerateMapIDParams,
+  GhostGetPendingParams,
+  GhostRejectPendingParams,
+  GhostTransferOwnershipParams,
+  GhostGetAllParams,
+  GhostAllowLinkParams,
+  GhostDenyLinkParams,
+  GhostStatusParams,
+  GhostMorphedAddParams,
+  GhostMorphedRemoveParams,
+  GhostReadAccessAddParams,
+  GhostReadAccessRemoveParams,
+} from './ghost/types.js'
 
 const createHiveClient = (hiveURL: string) => ({
   ...createHiveAuth(hiveURL),
@@ -11,3 +46,35 @@ const createHiveClient = (hiveURL: string) => ({
 })
 
 export default createHiveClient
+
+export type {
+  AuthGetTokenParams,
+  DomainGetAllParams,
+  DomainCreateParams,
+  DomainDeleteParams,
+  DomainActivateParams,
+  DomainDeactivateParams,
+  ServiceCreateParams,
+  ServiceDeleteParams,
+  ServiceGetAllParams,
+  ServiceGhostAddParams,
+  ServiceLinksParams,
+  ServiceLinksUpdateParams,
+  ServiceUpdateParams,
+  GhostAcceptPendingParams,
+  GhostAddMapIDParams,
+  GhostDeleteParams,
+  GhostDisableDataParams,
+  GhostGenerateMapIDParams,
+  GhostGetPendingParams,
+  GhostRejectPendingParams,
+  GhostTransferOwnershipParams,
+  GhostGetAllParams,
+  GhostAllowLinkParams,
+  GhostDenyLinkParams,
+  GhostStatusParams,
+  GhostMorphedAddParams,
+  GhostMorphedRemoveParams,
+  GhostReadAccessAddParams,
+  GhostReadAccessRemoveParams,
+}
